95eff5a60f653db69ac07f299f4a631a907fcccf
electricmoth <74592516+electricmoth@users.noreply.github.com>
2023-10-02 15:56:40 -0700

    exception handling, layout changes

--- main.py
+++ main.py
@@ -83,16 +83,18 @@ class Interface(ttk.Window):
         super().__init__(themename="vapor")  # more themes: darkly, journal, cyborg,
         # set title of window
         self.title("movie weasel")
-        # main "Add New Film" label
 
     def add_screen(self):
         """displays widgets on screen that allow user to enter film data"""
+        # clear labels/buttons/inputs from screen
         self.clear_widgets()
+        # main "Add New Film" label
         self.main_label = ttk.Label(text="Add New Film", font=("Calibri", 24, "bold"))
-        self.main_label.grid(column=1, row=0, pady=10)
+        self.main_label.grid(column=0, row=0, columnspan=2, pady=30)
         # label for 'film title' input box
         self.title_label = ttk.Label(text="Film Title", font='Calibri 14')
         self.title_label.grid(column=0, row=1, pady=10)
+        # cursor in this field on launch
         self.title_label.focus_set()
         # label for 'year released' input box
         self.year_label = ttk.Label(text="Year Released", font='Calibri 14')
@@ -100,52 +102,48 @@ class Interface(ttk.Window):
         # label for 'director' input box
         self.director_label = tk.Label(text="Director", font="Calibri 14")
         self.director_label.grid(column=0, row=3, pady=10)
-        # label for 'rating' dropdown menu
-        self.rating_label = tk.Label(text="Rating", font=("Calibri", 14,))
-        self.rating_label.grid(column=0, row=5, pady=10)
         # label for 'watched' toggle button
         self.watched_label = tk.Label(text="Watched", font=("Calibri", 14,))
         self.watched_label.grid(column=0, row=4, pady=10)
+        # label for 'rating' dropdown menu
+        self.rating_label = tk.Label(text="Rating", font=("Calibri", 14,))
+        self.rating_label.grid(column=0, row=5, pady=10)
         # label for 'comments' input box
         self.comments_label = tk.Label(text="Comments", font=("Calibri", 14))
-        self.comments_label.grid(column=0, row=6, pady=10, padx=20)
+        self.comments_label.grid(column=0, row=6, pady=10, padx=30)
         # -------- INPUTS ------------------
         #title input box
         self.title_input = ttk.Entry(width=20)
-        self.title_input.grid(column=2, row=1, pady=10)
+        self.title_input.grid(column=1, row=1, pady=10)
         self.title_input.focus()
         # year input box
         self.year_input = ttk.Entry(width=20)
-        self.year_input.grid(column=2, row=2, pady=10)
+        self.year_input.grid(column=1, row=2, pady=10)
         # director input box
         self.director_input = ttk.Entry(width=20)
-        self.director_input.grid(column=2, row=3, pady=10)
+        self.director_input.grid(column=1, row=3, pady=10)
+        # watched toggle button
+        self.watched_input = ttk.Checkbutton(bootstyle="success-square-toggle")
+        self.watched_input.grid(column=1, row=4)
         # rating dropdown menu
         # rating can be 1-5
         self.ratings = [1, 2, 3, 4, 5]
         self.rating_var = tk.IntVar()
         self.rating_input = tk.OptionMenu(self, self.rating_var, *self.ratings)
-        self.rating_input.grid(column=2, row=5, pady=10)
-        # watched toggle button
-        self.watched_input = ttk.Checkbutton(bootstyle="success-square-toggle")
-        self.watched_input.grid(column=2, row=4)
+        self.rating_input.grid(column=1, row=5, pady=10)
         # comments input box
         self.comments_input = ttk.Entry(width=30)
-        self.comments_input.grid(column=2, row=6, pady=10, padx=20)
+        self.comments_input.grid(column=1, row=6, pady=10, padx=20)
         # submit button
         self.submit_button = ttk.Button(text="Submit", command=self.submit, bootstyle="light")
-        self.submit_button.grid(column=1, row=7, pady=10, padx=20, sticky='EW')
-        # search input box
-        # self.search_input = ttk.Entry(width=20)
-        # self.search_input.grid(column=0, row=9, pady=10, padx=20, sticky='ew')
-        # search button
-        # command=partial(db.query, self.search_input.get()),
-        #self.search_button = ttk.Button(text="search", command=partial(db.query, (self.search_input.get(),)), bootstyle="info-outline")
-        self.search_button = ttk.Button(text="show data", command=self.show_data, bootstyle="secondary-outline")
-        self.search_button.grid(column=1, row=9, pady=10)
+        self.submit_button.grid(column=0, row=7, columnspan=2, pady=10, padx=30, sticky='EW')
+        # show data button
+        self.show_data_button = ttk.Button(text="Show Data", command=self.show_data, bootstyle="secondary-outline")
+        self.show_data_button.grid(column=0, columnspan=2, sticky='we', row=9, pady=20, padx=20)
+
         # separator line to section off the search part
         self.sep = ttk.Separator(self, orient='horizontal', style="light")
-        self.sep.grid(row=8, columnspan=3, pady=20, padx=20, sticky='EW')
+        self.sep.grid(row=8, columnspan=3, pady=30, padx=30, sticky='EW')
 
     def clear_widgets(self):
         """clears all widgets from screen"""
@@ -163,6 +161,12 @@ class Interface(ttk.Window):
         watched = self.watched_input.instate(['selected'])
         # check what the rating dropdown was set to
         rating = self.rating_var.get()
+
+        # validate user input
+        if not title:
+            messagebox.showerror("Validation Error", "Title field is required.")
+            return
+
         # create a new instance of Film class
         film = Film(title=title, year=year, director=director, rating=rating, watched=watched, comments=comments)
         # save data
@@ -189,11 +193,14 @@ class Interface(ttk.Window):
        self.home_button.grid(column=0, row=0, pady=10, padx=20, sticky='W')
 
        try:
+           # try to read data from database
            films = db.cur.execute("SELECT * FROM film").fetchall()
        except sqlite3.Error as e:
+           # catch errors
            print(e)
            self.error_message()
        else:
+           # get data for columns
            coldata = [
                 {"text": "title", "stretch": True},
                 {"text": "year", "stretch": False, "width": 55},
@@ -212,27 +219,28 @@ class Interface(ttk.Window):
                 searchable=True,
                 bootstyle=PRIMARY,
             )
-           dt.grid(padx=10, pady=10, column=0, row=1)
+           dt.grid(padx=20, pady=20, column=0, row=1)
            dt.focus_set()
 
 
 # -------- MAIN LOOP -------------------
 # main program
 if __name__ == "__main__":
-  try:
-    # initialize database
-    db = Database()
-    # create table in database
-    db.create_database()
-    # create GUI window
-    window = Interface()
-    # show add new film screen
-    window.add_screen()
-    # keep window open
-    window.mainloop()
-    # close database
-    db.close()
+    try:
+        # initialize database
+        db = Database()
+        # create table in database
+        db.create_database()
+        # create GUI window
+        window = Interface()
+        # show add new film screen
+        window.add_screen()
+        # keep window open
+        window.mainloop()
+        # close database
+        db.cur.close()
+        db.conn.close()
   # exit if program receives keyboard interrupt/ control-C
-  except KeyboardInterrupt:
-    print("\nGoodbye.\n")
+    except KeyboardInterrupt:
+        print("\nGoodbye.\n")
 
