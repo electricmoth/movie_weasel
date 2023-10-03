35c04bc78fd4596118aaa9e0698973d1802bf641
electricmoth <74592516+electricmoth@users.noreply.github.com>
2023-10-03 15:48:25 -0700

    modify exit

--- main.py
+++ main.py
@@ -6,6 +6,7 @@
 # sql database setup
 import sqlite3
 
+import sys
 # styling and components for gui interface
 from tkinter import messagebox
 import tkinter as tk
@@ -13,42 +14,7 @@ import ttkbootstrap as ttk
 from ttkbootstrap.constants import *
 from ttkbootstrap.tableview import Tableview
 from film import Film
-
-
-class Database:
-    def __init__(self):
-        """initialize a new Database instance"""
-        # create connection to database
-        with sqlite3.connect('films.db') as self.conn:
-            # Create a cursor object
-            self.cur = self.conn.cursor()
-
-    def create_database(self):
-        """create database if it does not already exist"""
-        self.cur.execute("CREATE TABLE IF NOT EXISTS film(title TEXT, year TEXT, director TEXT, rating INTEGER, watched INTEGER, comments TEXT)")
-        print("[+] database created.")
-
-    def insert_film(self, f: Film):
-        """add new film to database"""
-        # database fields are: id, title, yr, dir, rating, watched, comments
-        data = (
-            {
-                "title": f.title,
-                "year": f.year,
-                "director": f.director,
-                "rating": f.rating,
-                "watched": f.watched,
-                "comments": f.comments,
-             }
-                )
-        # insert data into database
-        try:
-            self.cur.execute("INSERT INTO film VALUES(:title, :year, :director, :rating, :watched, :comments)", data)
-        except sqlite3.Error as e:
-            print(f"Database error: {e}")
-        else:
-            # save changes
-            self.conn.commit()
+from database import Database
 
 
 class Interface(ttk.Window):
@@ -221,3 +187,4 @@ if __name__ == "__main__":
     # exit if program receives keyboard interrupt/ control-C
     except KeyboardInterrupt:
         print("\nGoodbye.\n")
+        sys.exit(0)
